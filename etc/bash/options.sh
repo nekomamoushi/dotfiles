742f371041c97612533c4fcccdb7cca55aaca2c8
Germione <germione.grangere@gmail.com>
2019-05-27 17:16:35 +0200

    feat(etc): add bash configuration

---
+++ etc/bash/options.sh
@@ -0,0 +1,19 @@
+
+# Check the window size after each command and, if necessary, update
+# the values of `LINES` and `COLUMNS`.
+shopt -s checkwinsize
+
+# Save all lines of a multiple-line command in the same history entry.
+shopt -s cmdhist
+
+# Include filenames beginning with a "." in the filename expansion.
+shopt -s dotglob
+
+# Use extended pattern matching features.
+shopt -s extglob
+
+# Append to the history file rather then overwriting it.
+shopt -s histappend
+
+# Auto cd
+shopt -s autocd
